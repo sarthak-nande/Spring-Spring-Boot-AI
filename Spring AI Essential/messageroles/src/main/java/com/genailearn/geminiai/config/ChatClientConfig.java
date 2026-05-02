c5085cfdf0dd7cf18e9d093ab611568e8d395ea5
sarthu210 <sarthu102@gmail.com>
2026-05-03 00:58:33 +0530

    Chat Options Handeled

--- Spring AI Essential/messageroles/src/main/java/com/genailearn/geminiai/config/ChatClientConfig.java
+++ Spring AI Essential/messageroles/src/main/java/com/genailearn/geminiai/config/ChatClientConfig.java
@@ -1,6 +1,9 @@
 package com.genailearn.geminiai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.google.genai.GoogleGenAiChatModel.ChatModel;
+import org.springframework.ai.google.genai.GoogleGenAiChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,15 +11,24 @@ import com.genailearn.geminiai.adviosrs.TokenUsageAuditAdvioser;
 
 @Configuration
 public class ChatClientConfig {
-	
+
 	@Bean
 	public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
-		return chatClientBuilder.defaultAdvisors(new TokenUsageAuditAdvioser()).defaultSystem("""
-		        You are an empathetic, professional, and knowledgeable Human Resources (HR) Assistant. 
-		        Your primary role is to assist employees with questions regarding company policies, benefits, payroll, leave management, and onboarding processes. 
-		        Always maintain a polite, neutral, and supportive tone. 
-		        Do not provide legal advice. If a query requires human intervention or involves sensitive employee relations, politely direct the employee to contact the HR department.
-		        """).build();
+		
+		//You Can Also Use To Handel Chat Options
+		ChatOptions chatOptions = ChatOptions.builder().model("").maxTokens(100).temperature(45.0).build();
+		
+		return chatClientBuilder
+				.defaultOptions(GoogleGenAiChatOptions.builder().model(ChatModel.GEMINI_1_5_FLASH).build())
+				.defaultAdvisors(new TokenUsageAuditAdvioser())
+				.defaultSystem(
+						"""
+								You are an empathetic, professional, and knowledgeable Human Resources (HR) Assistant.
+								Your primary role is to assist employees with questions regarding company policies, benefits, payroll, leave management, and onboarding processes.
+								Always maintain a polite, neutral, and supportive tone.
+								Do not provide legal advice. If a query requires human intervention or involves sensitive employee relations, politely direct the employee to contact the HR department.
+								""")
+				.build();
 	}
 
 }
