2d8202a0b92bc496519771ff8780f3bc2f45f533
sarthu210 <sarthu102@gmail.com>
2026-05-14 22:29:05 +0530

    Rag Initialized

---
+++ Rag Unlished/implementrag/src/main/java/com/ragimplementation/implementrag/ImplementragApplication.java
@@ -0,0 +1,13 @@
+package com.ragimplementation.implementrag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementragApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplementragApplication.class, args);
+	}
+
+}
---
+++ Rag Unlished/implementrag/src/test/java/com/ragimplementation/implementrag/ImplementragApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ragimplementation.implementrag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementragApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
