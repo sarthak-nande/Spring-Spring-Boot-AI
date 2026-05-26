b43a9bfc15fd47d8dd678a161c5afcdd39abed39
sarthu210 <sarthu102@gmail.com>
2026-05-26 19:59:18 +0530

    Started Learning Spring MCP

---
+++ learnmcp/src/test/java/com/springmcp/learnmcp/LearnmcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmcp.learnmcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnmcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
