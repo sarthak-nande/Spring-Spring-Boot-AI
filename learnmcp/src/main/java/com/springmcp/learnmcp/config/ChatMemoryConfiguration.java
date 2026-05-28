6c3f6f0b41f299fdb2e42897bd160fa7e9a5971e
sarthu210 <sarthu102@gmail.com>
2026-05-28 11:39:32 +0530

    ChatContoller Creadted

--- learnmcp/src/main/java/com/springmcp/learnmcp/config/ChatMemoryConfiguration.java
+++ learnmcp/src/main/java/com/springmcp/learnmcp/config/ChatMemoryConfiguration.java
@@ -3,12 +3,14 @@ package com.springmcp.learnmcp.config;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.mongo.MongoChatMemoryRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
 public class ChatMemoryConfiguration {
 
+	@Bean
 	public ChatMemory chatMemory(MongoTemplate mongoTemplate) {
 		MongoChatMemoryRepository mongoChatMemoryRepository = MongoChatMemoryRepository.builder()
 				.mongoTemplate(mongoTemplate)
--- learnmcp/src/main/java/com/springmcp/learnmcp/controller/ChatController.java
+++ learnmcp/src/main/java/com/springmcp/learnmcp/controller/ChatController.java
@@ -1,10 +1,34 @@
 package com.springmcp.learnmcp.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
 
 @RestController
 @RequestMapping("/api")
 public class ChatController {
+	
+	private final ChatClient chatClient;
+	
+	@Autowired
+	public ChatController(ChatClient chatClient) {
+		this.chatClient = chatClient;
+	}
+	
+	@GetMapping("/chat")
+	public String chat(@RequestParam String message, @RequestHeader String username) {
+		return chatClient
+				.prompt()
+				.advisors(advisorSpec -> advisorSpec.param(CONVERSATION_ID, username))
+				.user(message)
+				.call()
+				.content();
+	}
 
 }
