e761edd908e84468365e239715d71d79ec0119c0
sarthu210 <sarthu102@gmail.com>
2026-06-09 11:39:52 +0530

    mcpclient: McpCLientChatController Created!

---
+++ mcp/mcpclient/src/main/java/com/mcp/mcpclient/controller/McpClientChatController.java
@@ -0,0 +1,25 @@
+package com.mcp.mcpclient.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api")
+public class McpClientChatController {
+	
+	private final ChatClient chatClient;
+	
+	public McpClientChatController(ChatClient chatClient) {
+		this.chatClient = chatClient;
+	}
+	
+	@GetMapping("/chat")
+	public String chat(@RequestParam String message) {
+		return chatClient.prompt().user(message).call().content();
+	}
+
+}
